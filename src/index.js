1c58f659c524264a17f0cc090ab3ca1d894906b4
Luca Di Molfetta <63596667+dimolf345@users.noreply.github.com>
2021-10-20 20:34:24 +0200

    Almost completed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-import bootstrap from "bootstrap";
+import * as bootstrap from "bootstrap";
+
+let toggler = document.getElementById("navbar-toggler");
+let header = document.getElementsByTagName("header")[0];
+let navLinks = document.querySelectorAll("#navbarNav .nav-link");
+let navExpanded = false;
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", function () {
+    if (navExpanded) {
+      toggler.click();
+    } else return;
+  });
+});
+
+toggler.addEventListener("click", function () {
+  header.classList.toggle("header--mobile");
+  navExpanded = !navExpanded;
+  toggler.lastElementChild.className = navExpanded
+    ? "navbar-toggler-icon-dark"
+    : "navbar-toggler-icon";
+});
+
+window.addEventListener("resize", function () {
+  if (this.window.innerWidth > 768 && navExpanded) {
+    toggler.click();
+  } else return;
+});
